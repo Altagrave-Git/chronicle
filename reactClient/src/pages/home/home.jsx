5d11e7e6a6e8ac89073d44714388060bc6e7c2dc
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-05-05 16:12:27 -0600

    Add initial bio

--- reactClient/src/pages/home/home.jsx
+++ reactClient/src/pages/home/home.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import './home.scss';
-import GitStream from "../../components/gitstream/gitstream";
+import Aside from "../../components/aside/aside";
 
 const HomeView = ({gitData}) => {
 
   return (
     <main className='home container'>
-      <GitStream gitData={gitData} />
+      <section className="bio">
+        <h1 className="name title">Damon Turcotte</h1>
+        <h2 className="job subtitle mb-1">Full-stack Web Developer</h2>
+        <p>
+          I am a self-taught developer with a broad range of technical skills, ranging from UI design and scripting to database management and server configuration. I specialize in functional programming, because I like to have clear, readable and fully customizable code.
+        </p>
+      </section>
+      <Aside gitData={gitData} />
     </main>
   )
 };
