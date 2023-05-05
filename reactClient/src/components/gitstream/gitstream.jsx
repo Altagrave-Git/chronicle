b9506dd41586c407efa048212e8ad75aeb95ea4e
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-05-05 16:10:09 -0600

    Add repo cards and user data to component

--- reactClient/src/components/gitstream/gitstream.jsx
+++ reactClient/src/components/gitstream/gitstream.jsx
@@ -2,21 +2,32 @@ import './gitstream.scss';
 
 const GitStream = ({gitData}) => {
   return (
-    <div className='gitstream'>
-      { gitData.length === 0 ? <h2>Loading...</h2> :
-        gitData.map((repo, index) => {
-          return (
-            <div key={index} className='repo'>
-              <h3>{repo.name}</h3>
-              <p>{repo.description}</p>
-              <p>{repo.language}</p>
-              <p>{repo.updated_at}</p>
-              <a href={repo.html_url} target='_blank' rel='noreferrer'>View on GitHub</a>
-            </div>
-          )
-        })
-      }
-    </div>
+  <>
+    { gitData.length === 0 ? <h2>Loading...</h2> :
+      <div className='gitstream text-center pb-1'>
+        <h2 className='git-title title-2'>Github Stream</h2>
+        <a href='https://github.com/Altagrave-Git' className="git-user">
+          <img className='git-logo' src="https://avatars.githubusercontent.com/u/114028969?v=4" alt='Altagrave-Git Logo' />
+          <h3 className='subtitle-3'>Altagrave-Git</h3>
+        </a>
+        <h3 className='git-subtitle subtitle-2 text-left'>Recent Activity</h3>
+        {
+          gitData.map((repo, index) => {
+            { if (index < 5) {
+              return (
+                <a key={index} href={repo.html_url} target='_blank' rel='noreferrer'>
+                  <div className='git-repo text-left'>
+                    <h3 className="name text">{repo.name}</h3>
+                    <p className="date text-2">{repo.pushed_at.split("T")[0].replaceAll("-", "/")}</p>
+                  </div>
+                </a>
+              )
+            }}
+          })
+        }
+      </div>
+    }
+  </>
   )
 }
 
