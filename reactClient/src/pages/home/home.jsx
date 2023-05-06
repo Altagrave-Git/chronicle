de39d4a836f10e5e906191655cb6f534b713bcd4
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-05-06 01:12:30 -0600

    Add placeholder for page layout

--- reactClient/src/pages/home/home.jsx
+++ reactClient/src/pages/home/home.jsx
@@ -6,13 +6,14 @@ const HomeView = ({gitData}) => {
 
   return (
     <main className='home container'>
-      <section className="bio">
+      <section className="bio fixed">
         <h1 className="name title">Damon Turcotte</h1>
-        <h2 className="job subtitle mb-1">Full-stack Web Developer</h2>
+        <h2 className="job mb-1">Web Developer</h2>
         <p>
           I am a self-taught developer with a broad range of technical skills, ranging from UI design and scripting to database management and server configuration. I specialize in functional programming, because I like to have clear, readable and fully customizable code.
         </p>
       </section>
+      <section className="placeholder"></section>
       <Aside gitData={gitData} />
     </main>
   )
