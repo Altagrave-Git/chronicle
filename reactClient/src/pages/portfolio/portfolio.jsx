03a7c6459a62fd099f37431008027a2c30fc21fd
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-05-08 02:09:13 -0600

    Add initial portfolio ajax and card

--- reactClient/src/pages/portfolio/portfolio.jsx
+++ reactClient/src/pages/portfolio/portfolio.jsx
@@ -1,10 +1,34 @@
-import Aside from "../../components/aside/aside";
+import Content from "../../components/content/content";
+import { useState, useEffect } from 'react';
+import ProjectCard from "../../components/projectcard/projectcard";
+import Project from '../../components/project/project';
 
-const PortfolioView = ({gitData}) => {
+const PortfolioView = ({ gitData }) => {
+
+  const [portfolioData, setPortfolioData] = useState([]);
+
+  useEffect(() => {
+    fetch('http://127.0.0.1:8000/portfolio')
+      .then(response => response.json())
+      .then(data => setPortfolioData(data));
+  }, []);
+
+  console.log(portfolioData)
+
   return (
     <main>
-      <section>.</section>
-      <Aside gitData={gitData} />
+      <section>
+        <div className="portfolio">
+          {portfolioData &&
+            portfolioData.map((project, index) => {
+              return (
+                <ProjectCard key={index} project={project} />
+              )
+            })
+          }
+        </div>
+      </section>
+      <Content />
     </main>
   )
 }
