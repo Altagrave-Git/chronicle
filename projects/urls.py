be8015a45d9960b1a961a7e94e6fe4ddf070afb5
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-05-12 20:07:55 -0600

    Add new portfolio endpoints

--- projects/urls.py
+++ projects/urls.py
@@ -1,11 +1,19 @@
 from django.urls import path
-from .views import projects, project, apps, app, project_images, project_image
+from .views import projects, project, project_sections, project_section, project_images, project_image, apps, app, app_sections, app_section, app_images, app_image, snippets, snippet
 
 urlpatterns = [
     path('', projects, name='projects'),
-    path('<str:project_name>/', project, name='project'),
-    path('<str:project_name>/apps/', apps, name='apps'),
-    path('<str:project_name>/apps/<str:app_name>/', app, name='app'),
-    path('<str:project_name>/images/', project_images, name='project_images'),
-    path('<str:project_name>/images/<int:image_id>/', project_image, name='project_image'),
+    path('<int:project_id>/', project, name='project'),
+    path('<int:project_id>/sections/', project_sections, name='project_sections'),
+    path('<int:project_id>/sections/<int:section_id>/', project_section, name='project_section'),
+    path('<int:project_id>/images/', project_images, name='project_images'),
+    path('<int:project_id>/images/<int:image_id>/', project_image, name='project_image'),
+    path('<int:project_id>/apps/', apps, name='apps'),
+    path('<int:project_id>/apps/<int:app_id>/', app, name='app'),
+    path('<int:project_id>/apps/<int:app_id>/sections/', app_sections, name='app_sections'),
+    path('<int:project_id>/apps/<int:app_id>/sections/<int:section_id>/', app_section, name='app_section'),
+    path('<int:project_id>/apps/<int:app_id>/images/', app_images, name='app_images'),
+    path('<int:project_id>/apps/<int:app_id>/images/<int:image_id>/', app_image, name='app_image'),
+    path('<int:project_id>/code/', snippets, name='snippets'),
+    path('<int:project_id>/code/<int:snippet_id>/', snippet, name='snippet'),
 ]
