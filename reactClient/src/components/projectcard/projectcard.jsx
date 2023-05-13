d908ea81842497cfa30710c8f3193b66c5edbc0e
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-05-12 20:10:05 -0600

    Remove extraneous data

--- reactClient/src/components/projectcard/projectcard.jsx
+++ reactClient/src/components/projectcard/projectcard.jsx
@@ -1,41 +1,12 @@
 import './projectcard.scss'
 
-/*
-project = {
-  name: 'Project Title',
-  category: 'Project Category',
-  description: 'Project Description',
-  site: 'Project Site',
-  repo: 'Project Repo',
-  images: [
-    {
-      image: imageUrl,
-      project: 'Project Title'
-      app: 'App Name'
-    }]
-  apps: [
-    {
-      name: 'App Name',
-      description: 'App Description',
-      project: 'Project Title',
-      site: 'App Site',
-      images: [
-        {
-          image: imageUrl,
-          project: 'Project Title'
-          app: 'App Name'
-        }]
-    }]
-}
-*/
-
-const ProjectCard = ({ project, click }) => {
+const ProjectCard = ({ project, ordering, click }) => {
   const { name, category, description, site, repo, images, apps } = project;
   const baseUrl = 'http://127.0.0.1:8000';
 
   return (
-    <div className="project-card">
-      <div className="project-card__image" onClick={() => click(project)}>
+    <div onClick={() => click()} className={'project-card' + ' ' + ordering}>
+      <div className="project-card__image">
         <img src={baseUrl + images[0].image} alt={images[0].project} />
       </div>
       <div className="project-card__header">
@@ -43,10 +14,6 @@ const ProjectCard = ({ project, click }) => {
         <h3 className="project-card__category">{category}</h3>
       </div>
       <div className="project-card__body">
-        <div className="project-card__links">
-          <a href={site} className="project-card__link">Site</a>
-          <a href={repo} className="project-card__link">Repo</a>
-        </div>
       </div>
     </div>
   )
