9ddcd7306dad2be7a82a84a498d1184580b7025c
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-05-12 20:08:57 -0600

    Add user serializers

---
+++ users/serializers.py
@@ -0,0 +1,47 @@
+from rest_framework import serializers
+from .models import User
+from django.contrib.auth import authenticate
+from django.contrib.auth.hashers import make_password
+
+
+class UserRegisterSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+    password2 = serializers.CharField()
+
+    def validate(self, data):
+        if data['password'] != data['password2']:
+            raise serializers.ValidationError('Passwords must match')
+        return data
+    
+    def create(self, validated_data):
+        user = User.objects.create(
+            username=validated_data['username'],
+            password=make_password(validated_data['password'])
+        )
+        return user
+    
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'password2']
+
+
+class UserLoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    def validate(self, data):
+        user = authenticate(username=data['username'], password=data['password'])
+        if user:
+            return user
+        raise serializers.ValidationError('Incorrect Credentials')
+
+    class Meta:
+        model = User
+        fields = ['username', 'password']
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'is_staff']
