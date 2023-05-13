f2c1a3131e45438a6ae7c3401e70a0e2c7150c81
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-05-12 20:05:53 -0600

    Add endpoints to api homepage

--- djangoServer/views.py
+++ djangoServer/views.py
@@ -1,9 +1,42 @@
 from rest_framework.response import Response
 from rest_framework import request
 from rest_framework.decorators import api_view
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+
 
 @api_view(['GET'])
 def chronicle(request):
-    return Response({
-        'message': 'Welcome to Chronicle API'
-    })
+    data = [
+        'Hello, welcome to chronicle API',
+        {
+            'auth': False
+        },
+        {
+        'endpoints': {
+            'main': '/api/',
+            'projects': '/api/projects/',
+            'project': '/api/projects/:id/',
+            'project_sections': '/api/projects/:id/sections/',
+            'project_section': '/api/projects/:id/sections/:id/',
+            'project_images': '/api/projects/:id/images/',
+            'project_image': '/api/projects/:id/images/:id/',
+            'apps': '/api/projects/:id/apps/',
+            'app': '/api/projects/:id/apps/:id/',
+            'app_sections': '/api/projects/:id/apps/:id/sections/',
+            'app_section': '/api/projects/:id/apps/:id/sections/:id/',
+            'app_images': '/api/projects/:id/apps/:id/images/',
+            'app_image': '/api/projects/:id/apps/:id/images/:id/',
+            'snippets': '/api/projects/:id/code/',
+            'snippet': '/api/projects/:id/code/:id/',
+        }}
+    ]
+    if request.user.is_staff:
+        data[1]['auth'] = True
+
+    return Response(data)
