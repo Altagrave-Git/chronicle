9743806af5bdad698d3f28be525f130324f95e65
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-05-12 20:09:33 -0600

    Add temporary slider

--- reactClient/src/pages/blog/blog.jsx
+++ reactClient/src/pages/blog/blog.jsx
@@ -1,9 +1,12 @@
+import SliderComponent from '../../components/slider/slider';
+
 const BlogView = () => {
   return (
     <main>
       <section>.</section>
       <aside className="blog">
         <h1>Blog</h1>
+        <SliderComponent />
       </aside>
     </main>
   );
