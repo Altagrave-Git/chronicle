0aef011b2df9ecd0e32549c858c26f48f80b7c9a
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-05-12 20:06:10 -0600

    Add all project models to admin

--- projects/admin.py
+++ projects/admin.py
@@ -3,5 +3,9 @@ from . import models
 
 
 admin.site.register(models.Project)
-admin.site.register(models.App)
+admin.site.register(models.ProjectSection)
 admin.site.register(models.ProjectImage)
+admin.site.register(models.App)
+admin.site.register(models.AppSection)
+admin.site.register(models.AppImage)
+admin.site.register(models.Snippet)
