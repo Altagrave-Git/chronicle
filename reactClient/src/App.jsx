693d213c1a30b25db3f8aad144d8dd4903449f2f
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-05-18 23:51:03 -0600

    Comment out broken octokit import

--- reactClient/src/App.jsx
+++ reactClient/src/App.jsx
@@ -5,27 +5,29 @@ import HomeView from './pages/home/home';
 import PortfolioView from './pages/portfolio/portfolio';
 import BlogView from './pages/blog/blog';
 import AboutView from './pages/about/about';
-import { Octokit } from "https://cdn.skypack.dev/octokit?dts";
 import { useState, useEffect } from 'react';
+// import { Octokit } from "https://cdn.skypack.dev/octokit";
 
-const apiToken = import.meta.env.VITE_GITHUB_TOKEN;
-const octokit = new Octokit({ auth: apiToken }, { userAgent: 'Altagrave-Git' });
+// const apiToken = import.meta.env.VITE_GITHUB_TOKEN;
+// const octokit = new Octokit({ auth: apiToken }, { userAgent: 'Altagrave-Git' });
 
 const App = () => {
   const [gitData, setGitData] = useState([]);
   const [auth, setAuth] = useState(false);
 
-  useEffect(() => {
-    octokit.request('GET /users/Altagrave-Git/repos', {
-      sort: 'pushed',
-    })
-      .then(({ data }) => {
-        setGitData(data);
-      });
-  }, []);
+  // useEffect(() => {
+  //   octokit.request('GET /users/Altagrave-Git/repos', {
+  //     sort: 'pushed',
+  //   })
+  //     .then(({ data }) => {
+  //       setGitData(data);
+  //     })
+  //     .catch(err => console.log(err));
+  // }, []);
 
   console.log(gitData);
 
+  // Check authorization level of current user session
   useEffect(() => {
     fetch(import.meta.env.VITE_CHRONICLE_URL)
       .then(response => response.json())
