a404b3f6ee1ac36ce79ef87b6467c88abe2f73fe
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-05-18 23:48:45 -0600

    Render returned API values

--- reactClient/src/components/projectdetail/projectdetail.jsx
+++ reactClient/src/components/projectdetail/projectdetail.jsx
@@ -1,30 +1,88 @@
+import './projectdetail.scss'
+import { ReactComponent as GithubLogo } from "../../icons/github.svg";
+import { ReactComponent as SiteLogo } from "../../icons/website.svg";
+import TechIcon from '../techicon/techicon';
+
 const ProjectDetail = ({ project }) => {
+
   return (
   <>
     { project &&
       <div className="project-detail">
         <div className="project-detail__header">
-          <h2 className="project-detail__title">{project.name}</h2>
-          <h3 className="project-detail__category">{project.category}</h3>
+          <div>
+            <h2 className="project-detail__title">{project.name}</h2>
+            <h3 className="project-detail__category">{project.category}</h3>
+          </div>
+          <div className="project-detail__links">
+            <a href={project.site} className="project-detail__link">
+              <SiteLogo />
+            </a>
+            <a href={project.repo} className="project-detail__link">
+              <GithubLogo />
+            </a>
+          </div>
         </div>
         <div className="project-detail__body">
           <div className="project-detail__description">
             <p>{project.description}</p>
           </div>
-          <div className="project-detail__links">
-            <a href={project.site} className="project-detail__link">Site</a>
-            <a href={project.repo} className="project-detail__link">Repo</a>
+          { project.tech &&
+            <div className="project-detail__section">
+              <h4>Technologies</h4>
+              <div className="project-detail__tech">
+                  {project.tech.map((tech, index) => {
+                    return (
+                      <div key={index} className="project-detail__tech-item">
+                        <TechIcon tech={tech.tech} />
+                        <span>{tech.tech}</span>
+                      </div>
+                    )
+                  })}
+              </div>
+            </div>
+          }
+          <div className="project-detail__sections">
+            {
+              project.sections && project.sections.map((section, index) => {
+                return (
+                  <div key={index} className="project-detail__section">
+                    <h4>{section.title}</h4>
+                    {section.type === 'text' &&
+                      <p>{section.description}</p>
+                    }
+                    { section.type === 'list' &&
+                      <ul>
+                        {section.description.split('\n').map((item, index) => {
+                          return <li key={index}>{item}</li>
+                        })}
+                      </ul>
+                    }
+                  </div>
+                )
+              })
+            }     
           </div>
         </div>
-        <div>
-            <h3 className="project-detail__apps">Apps</h3>
-            <ul className="project-detail__app-list">
-              {project.apps && project.apps.map((app, index) => {
+        <div className="project-detail__apps">
+            {project.apps &&
+              project.apps.map((app, index) => {
                 return (
-                  <li key={index} className="project-detail__app">{app.name}</li>
+                  <div key={index} className="project-detail__app">
+                    <h4>{app.name}</h4>
+                    <p>{app.description}</p>
+                    <div className="project-detail__app-images">
+                      { app.images &&
+                        app.images.map((image, index) => {
+                        return (
+                          <img key={index} src={image} alt={app.name} />
+                        )
+                      })}
+                    </div>
+                  </div>
                 )
-              })}
-            </ul>
+              })
+            }
         </div>
       </div>
     }
