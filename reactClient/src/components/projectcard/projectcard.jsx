d1172e3760b30118f195bebbf73c631375ec4665
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-05-18 23:50:42 -0600

    Remove body section from card

--- reactClient/src/components/projectcard/projectcard.jsx
+++ reactClient/src/components/projectcard/projectcard.jsx
@@ -5,15 +5,13 @@ const ProjectCard = ({ project, ordering, click }) => {
   const baseUrl = 'http://127.0.0.1:8000';
 
   return (
-    <div onClick={() => click()} className={'project-card' + ' ' + ordering}>
+    <div onClick={() => click(project)} className={'project-card' + ' ' + ordering}>
       <div className="project-card__image">
         <img src={baseUrl + images[0].image} alt={images[0].project} />
       </div>
       <div className="project-card__header">
         <h2 className="project-card__title">{name}</h2>
         <h3 className="project-card__category">{category}</h3>
-      </div>
-      <div className="project-card__body">
       </div>
     </div>
   )
