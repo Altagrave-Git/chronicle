4c69b39fd0794ad99e1b6ebb4f6f715caf7c17de
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-05-18 23:47:46 -0600

    Remove deprecated component

--- reactClient/src/pages/blog/blog.jsx
+++ reactClient/src/pages/blog/blog.jsx
@@ -1,12 +1,9 @@
-import SliderComponent from '../../components/slider/slider';
-
 const BlogView = () => {
   return (
     <main>
       <section>.</section>
       <aside className="blog">
         <h1>Blog</h1>
-        <SliderComponent />
       </aside>
     </main>
   );
