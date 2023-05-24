af6408dcbe4fd27a0b08100f4f5cf9a23d8dc8d0
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-05-23 20:44:11 -0600

    Change default permissions, auth classes

--- djangoServer/settings.py
+++ djangoServer/settings.py
@@ -110,10 +110,11 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.BrowsableAPIRenderer',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+        'rest_framework.permissions.AllowAny',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication',
+        # rest_framework.authentication.SessionAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ]
 }
 
