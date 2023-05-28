4a58e392b3eef985f9213838de1c0c3598aeeee8
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-05-28 15:50:58 -0600

    Add api index

--- djangoServer/views.py
+++ djangoServer/views.py
@@ -1,13 +1,6 @@
 from rest_framework.response import Response
-from rest_framework import request
 from rest_framework.decorators import api_view
-from django.shortcuts import render
-
-
-def index(request):
-    return render(request, 'index.html')
-
-
+from django.contrib.sessions.models import Session
 
 
 @api_view(['GET'])
@@ -19,24 +12,23 @@ def chronicle(request):
         },
         {
         'endpoints': {
-            'main': '/api/',
-            'projects': '/api/projects/',
-            'project': '/api/projects/:id/',
-            'project_sections': '/api/projects/:id/sections/',
-            'project_section': '/api/projects/:id/sections/:id/',
-            'project_images': '/api/projects/:id/images/',
-            'project_image': '/api/projects/:id/images/:id/',
-            'apps': '/api/projects/:id/apps/',
-            'app': '/api/projects/:id/apps/:id/',
-            'app_sections': '/api/projects/:id/apps/:id/sections/',
-            'app_section': '/api/projects/:id/apps/:id/sections/:id/',
-            'app_images': '/api/projects/:id/apps/:id/images/',
-            'app_image': '/api/projects/:id/apps/:id/images/:id/',
-            'snippets': '/api/projects/:id/code/',
-            'snippet': '/api/projects/:id/code/:id/',
+            'projects': '/projects/',
+            'project': '/projects/:id/',
+            'project_sections': '/projects/:id/sections/',
+            'project_section': '/projects/:id/sections/:id/',
+            'project_images': '/projects/:id/images/',
+            'project_image': '/projects/:id/images/:id/',
+            'apps': '/projects/:id/apps/',
+            'app': '/projects/:id/apps/:id/',
+            'app_sections': '/projects/:id/apps/:id/sections/',
+            'app_section': '/projects/:id/apps/:id/sections/:id/',
+            'app_images': '/projects/:id/apps/:id/images/',
+            'app_image': '/projects/:id/apps/:id/images/:id/',
+            'snippets': '/projects/:id/code/',
+            'snippet': '/projects/:id/code/:id/',
         }}
     ]
-    if request.user.is_staff:
+    if request.user.is_superuser:
         data[1]['auth'] = True
 
     return Response(data)
