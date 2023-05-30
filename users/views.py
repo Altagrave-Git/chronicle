af79567cbf9874c153d7e76ae1fada1030d4eabe
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-05-29 21:47:24 -0600

    Add new oauth2 encryption logic

--- users/views.py
+++ users/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth import login, logout
 from rest_framework.response import Response
 from rest_framework import status, permissions
 from users.models import User
-from rest_framework.decorators import permission_classes, api_view
+from rest_framework.decorators import permission_classes, api_view, renderer_classes, authentication_classes
 import requests
 import random
 import string
@@ -13,6 +13,7 @@ from djangoServer import settings
 import requests
 import jwt
 from django.utils import timezone
+from django.views.decorators.csrf import csrf_exempt
 
 CLIENT_ID = settings.CLIENT_ID
 CLIENT_SECRET = settings.CLIENT_SECRET
@@ -26,12 +27,14 @@ CODE_CHALLENGE = base64.urlsafe_b64encode(CODE_CHALLENGE).decode('utf-8').replac
 
 auth_url = "https://echonetwork.app/o/authorize/?response_type=code&code_challenge={}&code_challenge_method=S256&client_id={}&redirect_uri={}".format(CODE_CHALLENGE, CLIENT_ID, REDIRECT_URI)
 
+
 @permission_classes([permissions.AllowAny])
 @api_view(["GET", "POST"])
+@authentication_classes([])
 def auth_view(request):
     if request.method == "GET":
         # set the code verifier in the session for later use
-        verifier = jwt.encode(CODE_VERIFIER, CLIENT_SECRET, algorithm="HS256")
+        verifier = jwt.encode({"verifier": CODE_VERIFIER.decode('utf-8')}, CLIENT_SECRET, algorithm="HS256")
         request.session["code_verifier"] = verifier
         return Response({"auth_url": auth_url}, status=status.HTTP_200_OK)
     
@@ -39,7 +42,9 @@ def auth_view(request):
         # retrieve the authorization code from the request and the code verifier from the session
         code = request.data.get("code")
         verifier = request.session.get("code_verifier")
+        print(verifier)
         verifier = jwt.decode(verifier, CLIENT_SECRET, algorithms=["HS256"])
+        verifier = verifier.get("verifier").encode('utf-8')
         request.session["code_verifier"] = None
 
         if code and verifier:
@@ -60,52 +65,53 @@ def auth_view(request):
 
             auth_request = requests.post("https://echonetwork.app/o/token/", headers=headers, data=data)
 
-            try:
-                access_token = auth_request.json().get("access_token")
-                refresh_token = auth_request.json().get("refresh_token")
-                token_type = auth_request.json().get("token_type")
-                expires_in = auth_request.json().get("expires_in")
-                scope = auth_request.json().get("scope")
-                id_token = auth_request.json().get("id_token")
+            # pull tokens from the response
+            access_token = auth_request.json().get("access_token")
+            refresh_token = auth_request.json().get("refresh_token")
+            token_type = auth_request.json().get("token_type")
+            expires_in = auth_request.json().get("expires_in")
+            scope = auth_request.json().get("scope")
+            id_token = auth_request.json().get("id_token")
 
-                # decode the id token
-                decrypted_id_token = jwt.decode(
-                    id_token,
-                    key=CLIENT_SECRET,
-                    algorithms=["HS256"],
-                    options={"verify_signature": False}
-                )
+            # decode the id token
+            decrypted_id_token = jwt.decode(
+                id_token,
+                key=CLIENT_SECRET,
+                algorithms=["HS256"],
+                options={"verify_signature": False}
+            )
 
-                # hash the access token (HS256)
-                access_token_hash = hashlib.sha256(access_token.encode('utf-8')).digest()
+            # hash the access token (HS256)
+            access_token_hash = hashlib.sha256(access_token.encode('utf-8')).digest()
 
-                # take the first half of the hash
-                access_token_hash = access_token_hash[:len(access_token_hash)//2]
+            # take the first half of the hash
+            access_token_hash = access_token_hash[:len(access_token_hash)//2]
 
-                # base64 encode the access token hash
-                access_token_hash = base64.urlsafe_b64encode(access_token_hash).decode('utf-8').replace('=', '')
+            # base64 encode the access token hash
+            access_token_hash = base64.urlsafe_b64encode(access_token_hash).decode('utf-8').replace('=', '')
 
-                # verify the access token hash and audience
-                if access_token_hash != decrypted_id_token.get("at_hash"):
-                    return Response(data="Invalid access token hash", status=status.HTTP_400_BAD_REQUEST)
-                if decrypted_id_token.get("aud") != CLIENT_ID:
-                    return Response(data="Invalid client", status=status.HTTP_400_BAD_REQUEST)
+            # verify the access token hash and audience
+            if access_token_hash != decrypted_id_token.get("at_hash"):
+                return Response(data="Invalid access token hash", status=status.HTTP_400_BAD_REQUEST)
+            if decrypted_id_token.get("aud") != CLIENT_ID:
+                return Response(data="Invalid client", status=status.HTTP_400_BAD_REQUEST)
                 
-                # print the decrypted id token
-                print(decrypted_id_token)
+            # print the decrypted id token
+            print(decrypted_id_token)
 
-                # retrieve user data from the id token
-                username = decrypted_id_token.get("preferred_username")
-                first_name = decrypted_id_token.get("given_name")
-                last_name = decrypted_id_token.get("family_name")
-                email = decrypted_id_token.get("email")
-                avatar = decrypted_id_token.get("picture")
+            # retrieve user data from the id token
+            username = decrypted_id_token.get("preferred_username")
+            first_name = decrypted_id_token.get("given_name")
+            last_name = decrypted_id_token.get("family_name")
+            email = decrypted_id_token.get("email")
+            avatar = decrypted_id_token.get("picture")
 
-                # use the id token to create a temporary password for the user
-                active_session = hashlib.sha256(id_token.encode('utf-8')).hexdigest()
-                
-                # create or retrieve the user
-                user = User.objects.get_or_create(username=username)[0]
+            # use the id token to create a temporary password for the user
+            active_session = hashlib.sha256(id_token.encode('utf-8')).hexdigest()
+
+            # create or retrieve the user
+            if User.objects.filter(username=username).exists():
+                user = User.objects.get(username=username)
                 user.first_name = first_name
                 user.last_name = last_name
                 user.email = email
@@ -118,19 +124,34 @@ def auth_view(request):
                 user.active_session = active_session
                 user.save()
 
-                # set hashed id token in the session for later use
-                request.session["active_session"] = active_session
+            else:
+                user = User(
+                    username=username,
+                    first_name=first_name,
+                    last_name=last_name,
+                    email=email,
+                    avatar=avatar,
+                    access_token=access_token,
+                    refresh_token=refresh_token,
+                    token_type=token_type,
+                    expires_in=expires_in,
+                    scope=scope,
+                    active_session=active_session
+                )
+                user.save()
 
-                # login the user
-                login(request, user)
+            # login the user
+            login(request, user)
 
-                # return the user data
-                serializer = UserSerializer(user)
-                data = serializer.data
+            # set hashed id token in the session for later use
+            request.session["active_session"] = active_session
 
-                return Response(data=data, status=status.HTTP_200_OK)
-            except:
-                return Response(data=auth_request.text, status=status.HTTP_400_BAD_REQUEST)
+            # return the user data
+            serializer = UserSerializer(user)
+            data = serializer.data
+
+            return Response(data=data, status=status.HTTP_200_OK)
+            
         else:
             return Response(data="No code or verifier provided", status=status.HTTP_400_BAD_REQUEST)
         
@@ -142,11 +163,11 @@ def auth_view(request):
 @api_view(["GET"])
 def check_session(request):
     if request.method == "GET":
-        # retrieve the active session from the session
+        # retrieve the active session token from the session
         active_session = request.session.get("active_session")
 
         if active_session:
-            # retrieve the user from the active session
+            # retrieve the user from the active session token
             user = User.objects.get(active_session=active_session)
 
             # check if the session is still valid
