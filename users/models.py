2b7ea6812f1aa120d76a8161b6ca41b904905234
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-05-29 21:46:25 -0600

    Fix modified save method

--- users/models.py
+++ users/models.py
@@ -7,7 +7,7 @@ from datetime import timedelta
 
 class User(AbstractUser):
     access_token = models.CharField(max_length=255)
-    refresh_token = models.CharField(max_length=255)
+    refresh_token = models.CharField(max_length=255, blank=True, null=True)
     token_type = models.CharField(max_length=255)
     expires_in = models.IntegerField(default=0)
     expires_at = models.DateTimeField(blank=True, null=True)
@@ -18,14 +18,13 @@ class User(AbstractUser):
     last_name = models.CharField(max_length=255,  blank=True, null=True)
     avatar = models.URLField(blank=True, null=True)
     email = models.EmailField(blank=True, null=True)
-    password = None
 
     active_session = models.CharField(max_length=255, blank=True, null=True)
+    password = None
 
     def __str__(self):
         return self.username
     
     def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
         self.expires_at = timezone.now() + timedelta(seconds=self.expires_in)
-        self.save()
+        super(User, self).save(*args, **kwargs)
