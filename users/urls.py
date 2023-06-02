7733cb3f176f6d131f3d6dffe5a0cbe3ce701101
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-06-01 18:52:44 -0600

    Add logout url

--- users/urls.py
+++ users/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('auth/', views.auth_view, name='auth'),
     path('check/', views.check_session, name='check'),
+    path('logout/', views.logout_view, name="logout"),
 ]
