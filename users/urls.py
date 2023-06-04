e364904d9e25fd49ce8f36db15584f07f108ba1a
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-06-03 21:44:25 -0600

    Add introspection test url

--- users/urls.py
+++ users/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('auth/', views.auth_view, name='auth'),
     path('check/', views.check_session, name='check'),
     path('logout/', views.logout_view, name="logout"),
+    path('intro/', views.intro, name="introspect")
 ]
