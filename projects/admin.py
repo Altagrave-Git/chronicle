040c7b6a99bd07d5f85464ce9bc2b9318063a810
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-06-03 21:42:38 -0600

    Rm apps and rel models

--- projects/admin.py
+++ projects/admin.py
@@ -5,8 +5,5 @@ from . import models
 admin.site.register(models.Project)
 admin.site.register(models.ProjectSection)
 admin.site.register(models.ProjectImage)
-admin.site.register(models.App)
-admin.site.register(models.AppSection)
-admin.site.register(models.AppImage)
 admin.site.register(models.Technology)
 admin.site.register(models.Snippet)
