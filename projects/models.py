64cae35f0d9dfbff66169851fb84f092fd18e86d
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-06-03 21:43:06 -0600

    Change technology model, rm apps and related

--- projects/models.py
+++ projects/models.py
@@ -4,8 +4,37 @@ from pygments.styles import get_all_styles
 from pygments.formatters.html import HtmlFormatter
 from pygments import highlight
 
-class Technology(models.Model):    
-    tech = models.CharField(max_length=100)
+# class Meta:
+#     unique_together = ['id', 'order']
+#     ordering = ['order']
+
+class Technology(models.Model):
+    TECH_CHOICES = [
+        ('anaconda', 'Anaconda'),
+        ('auth0', 'Auth0'),
+        ('bash', 'Bash'),
+        ('bootstrap', 'Bootstrap'),
+        ('css', 'CSS'),
+        ('django', 'Django'),
+        ('git', 'Git'),
+        ('github', 'Github'),
+        ('gunicorn', 'Gunicorn'),
+        ('html', 'HTML'),
+        ('javascript', 'Javascript'),
+        ('jquery', 'jQuery'),
+        ('linux', 'Linux'),
+        ('nginx', 'NGINX'),
+        ('postgresql', 'PostgreSQL'),
+        ('python', 'Python'),
+        ('react', 'React'),
+        ('restapi', 'Rest API'),
+        ('restframework', 'Django Rest Framework'),
+        ('sass', 'SASS'),
+        ('sqlite', 'SQLite'),
+        ('ubuntu', 'Ubuntu'),
+    ]
+
+    tech = models.CharField(choices=TECH_CHOICES, default='python', max_length=50)
 
     def __str__(self):
         return self.tech
@@ -56,6 +85,7 @@ class ProjectImage(models.Model):
         return f'projects/{instance.project.name}/{filename}'
     
     TYPES = (
+        ('thumbnail', 'Thumbnail'),
         ('mobile-display', 'Mobile Display'),
         ('desktop-display', 'Desktop Display'),
         ('mobile-screenshot', 'Mobile Screenshot'),
@@ -64,79 +94,15 @@ class ProjectImage(models.Model):
         ('other', 'Other')
     )
 
-    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='images')
-    section = models.ForeignKey(ProjectSection, on_delete=models.CASCADE, related_name='images', null=True, blank=True)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='images', null=True, blank=True)
     image = models.ImageField(upload_to=get_image_path, default='projects/default.png')
     type = models.CharField(max_length=20, choices=TYPES, default='other')
 
     def __str__(self):
-        if self.section:
-            return self.project.name + ' - ' + self.section.title + ' - ' + str(self.image)
-        else:
-            return self.project.name + ' - ' + str(self.image)
+        return self.project.name + ' - ' + str(self.image)
     
     class Meta:
         ordering = ['project']
-
-
-class App(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField(max_length=1000)
-    site = models.URLField()
-    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='apps')
-
-    def __str__(self):
-        return self.project.name + ': ' + self.name
-    
-    class Meta:
-        ordering = ['name']
-
-
-class AppSection(models.Model):
-    SECTION_TYPES = (
-        ('list', 'List'),
-        ('text', 'Text'),
-        ('table', 'Table'),
-        ('other', 'Other')
-    )
-        
-    app = models.ForeignKey(App, on_delete=models.CASCADE, related_name='sections')
-    title = models.CharField(max_length=100)
-    description = models.TextField(max_length=1000, null=True, blank=True)
-    type = models.CharField(max_length=100, choices=SECTION_TYPES, default='other')
-    order = models.IntegerField(blank=True, null=True)
-
-    def __str__(self):
-        return self.app.project.name + ': ' + self.app.name + ' - ' + self.description[:50]
-    
-    class Meta:
-        ordering = ['app', 'order']
-
-
-class AppImage(models.Model):
-    def get_image_path(instance, filename):
-        return f'projects/{instance.project.name}/{instance.app.name}/{filename}'
-    
-    TYPES = (
-        ('card', 'Card'),
-        ('mobile-display', 'Mobile Display'),
-        ('desktop-display', 'Desktop Display'),
-        ('mobile-screenshot', 'Mobile Screenshot'),
-        ('desktop-screenshot', 'Desktop Screenshot'),
-        ('logo', 'Logo'),
-        ('other', 'Other')
-    )
-
-    app = models.ForeignKey(App, on_delete=models.CASCADE, related_name='images')
-    section = models.ForeignKey(AppSection, on_delete=models.CASCADE, related_name='images', null=True, blank=True)
-    image = models.ImageField(upload_to=get_image_path, default='projects/default.png')
-    type = models.CharField(max_length=20, choices=TYPES, default='other')
-
-    def __str__(self):
-        return self.app.project.name + ': ' + self.app.name + ' - ' + str(self.image)
-    
-    class Meta:
-        ordering = ['app']
 
 
 LEXERS = [item for item in get_all_lexers() if item[1]]
@@ -147,8 +113,6 @@ STYLE_CHOICES = sorted([(item, item) for item in get_all_styles()])
 class Snippet(models.Model):
     project = models.ForeignKey(Project, related_name='snippets', on_delete=models.CASCADE, null=True, blank=True)
     project_section = models.ForeignKey(ProjectSection, related_name='snippets', on_delete=models.CASCADE, null=True, blank=True)
-    app = models.ForeignKey(App, related_name='snippets', on_delete=models.CASCADE, null=True, blank=True)
-    app_section = models.ForeignKey(AppSection, related_name='snippets', on_delete=models.CASCADE, null=True, blank=True)
     title = models.CharField(max_length=100, blank=True, default='')
     code = models.TextField()
     highlighted = models.TextField(null=True, blank=True)
@@ -156,20 +120,13 @@ class Snippet(models.Model):
     style = models.CharField(choices=STYLE_CHOICES, default='monokai', max_length=100)
 
     class Meta:
-        ordering = ['project', 'app', 'project_section', 'app_section']
+        ordering = ['project', 'project_section']
         
     def __str__(self):
-        if self.project:
+        if self.project_section:
+            return f'{self.project} - {self.project_section} - {self.title}'
+        elif self.project:
             return f'{self.project} - {self.title}'
-        
-        elif self.project_section:
-            return f'{self.project_section} - {self.title}'
-        
-        elif self.app:
-            return f'{self.app} - {self.title}'
-        
-        elif self.app_section:
-            return f'{self.app_section} - {self.title}'
         
         return self.title
 
