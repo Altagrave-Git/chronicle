56abaf3d33f70366f352b5722e023579019cb068
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-06-06 23:17:37 -0600

    Rm apps and related urls

--- djangoServer/views.py
+++ djangoServer/views.py
@@ -8,9 +8,6 @@ def index(request):
     data = [
         'Hello, welcome to chronicle API',
         {
-            'auth': False
-        },
-        {
         'endpoints': {
             'projects': '/projects/',
             'project': '/projects/:id/',
@@ -18,17 +15,9 @@ def index(request):
             'project_section': '/projects/:id/sections/:id/',
             'project_images': '/projects/:id/images/',
             'project_image': '/projects/:id/images/:id/',
-            'apps': '/projects/:id/apps/',
-            'app': '/projects/:id/apps/:id/',
-            'app_sections': '/projects/:id/apps/:id/sections/',
-            'app_section': '/projects/:id/apps/:id/sections/:id/',
-            'app_images': '/projects/:id/apps/:id/images/',
-            'app_image': '/projects/:id/apps/:id/images/:id/',
             'snippets': '/projects/:id/code/',
             'snippet': '/projects/:id/code/:id/',
         }}
     ]
-    if request.user.is_superuser:
-        data[1]['auth'] = True
 
     return Response(data)
