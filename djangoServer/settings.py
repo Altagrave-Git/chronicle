f138def4142ca7a3bdbeaed7e3fc4345f498d1ee
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-06-13 16:38:12 -0600

    Set debug to environ

--- djangoServer/settings.py
+++ djangoServer/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-DEBUG = False
+DEBUG = os.environ.get('DEBUG', False)
 
 ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS'), os.environ.get('ALLOWED_HOSTS2'), 'localhost']
 
