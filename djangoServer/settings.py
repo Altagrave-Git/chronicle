96acd502959f95a59f17f94fd4be09c4042bd1eb
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-06-13 21:00:19 -0600

    Mod debug

--- djangoServer/settings.py
+++ djangoServer/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-DEBUG = os.environ.get('DEBUG', False)
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS'), os.environ.get('ALLOWED_HOSTS2'), 'localhost']
 
