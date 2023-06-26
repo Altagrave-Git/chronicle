90b0ed6b637378374a3a56837f1590cf3478b3ae
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-06-26 03:38:54 -0600

    Add models to admin view

--- projects/admin.py
+++ projects/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from . import models
 
-
 admin.site.register(models.Project)
 admin.site.register(models.ProjectSection)
 admin.site.register(models.ProjectImage)
+admin.site.register(models.ProjectVideo)
 admin.site.register(models.Technology)
 admin.site.register(models.Snippet)
