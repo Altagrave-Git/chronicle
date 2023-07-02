1b6f5a6a4fe38f18426bcaed1ec2a825ba7d613b
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-07-02 01:47:57 -0600

    Add update method to message serializer

--- contact/serializers.py
+++ contact/serializers.py
@@ -2,10 +2,12 @@ from rest_framework import serializers
 from .models import Message
 
 class MessageSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
     sender = serializers.CharField(max_length=100)
     contact = serializers.CharField(max_length=100)
     content = serializers.CharField(max_length=2000)
     timestamp = serializers.DateTimeField(read_only=True)
+    is_new = serializers.BooleanField(allow_null=True, required=False)
     date = serializers.SerializerMethodField(allow_null=True, read_only=True)
     time = serializers.SerializerMethodField(allow_null=True, read_only=True)
 
@@ -19,7 +21,15 @@ class MessageSerializer(serializers.Serializer):
 
     def create(self, validated_data):
         return Message.objects.create(**validated_data)
+    
+    def update(self, instance, **validated_data):
+        instance.sender = validated_data.get('sender', instance.sender)
+        instance.contact = validated_data.get('contact', instance.contact)
+        instance.content = validated_data.get('content', instance.content)
+        instance.is_new = validated_data.get('is_new', instance.is_new)
+        instance.save()
+        return instance
 
     class Meta:
         model = Message
-        fields = ['sender', 'contact', 'content', 'timestamp']
+        fields = ['sender', 'contact', 'content', 'is_new']
