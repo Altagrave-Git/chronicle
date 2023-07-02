73843e72f21473da375c926cd4891f8d3e34e475
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-07-02 01:48:25 -0600

    Add message and check urls

--- contact/urls.py
+++ contact/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.messages, name='messages'),
+    path('<int:id>/', views.message, name="message"),
     path('send/', views.send, name='send'),
+    path('check/', views.check, name="check_messages")
 ]
