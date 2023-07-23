ec55461254304157930c2c44cf310da5c8290d3e
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-07-22 21:29:05 -0600

    Add blog tables to admin

---
+++ blog/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from . import models
+
+admin.site.register(models.Post)
+admin.site.register(models.Paragraph)
+admin.site.register(models.Image)
+admin.site.register(models.Video)
+admin.site.register(models.Snippet)
+admin.site.register(models.Content)
+admin.site.register(models.Category)
+admin.site.register(models.Link)
