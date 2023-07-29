8df2e5e299f3e785a3c3dcd27c746a48e4815dce
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-07-29 05:47:08 -0600

    Rm unused files on delete, save

--- projects/models.py
+++ projects/models.py
@@ -3,6 +3,10 @@ from pygments.lexers import get_all_lexers, get_lexer_by_name
 from pygments.styles import get_all_styles
 from pygments.formatters.html import HtmlFormatter
 from pygments import highlight
+from main.utils import custom_img
+from main.settings import MEDIA_ROOT
+import os
+import shutil
 
 
 class Technology(models.Model):
@@ -17,15 +21,15 @@ class Technology(models.Model):
 
 class Project(models.Model):
     def get_image_path(instance, filename):
-        return f'projects/{instance.name}/thumbnail/{filename}'
+        return f'projects/{instance.name.replace(" ", "-")}/thumbnail/{filename}'
     
     def get_svg_path(instance, filename):
-        return f'projects/{instance.name}/logo/{filename}'
+        return f'projects/{instance.name.replace(" ", "-")}/logo/{filename}'
     
     name = models.CharField(max_length=100, unique=True)
     category = models.CharField(max_length=100)
     description = models.TextField(max_length=1000, null=True, blank=True)
-    tech = models.ManyToManyField(Technology, related_name='projects')
+    tech = models.ManyToManyField(Technology, related_name='projects', blank=True)
     site = models.URLField(blank=True, null=True)
     repo = models.URLField(blank=True, null=True)
     image = models.ImageField(upload_to=get_image_path, default='projects/default.png')
@@ -34,6 +38,41 @@ class Project(models.Model):
 
     def __str__(self):
         return self.name.replace(" ", "-")
+    
+    def save(self, *args, **kwargs):
+
+        if self.pk:
+            try:
+                prev_instance = Project.objects.get(pk=self.pk)
+                if self.image:
+                    if self.image != prev_instance.image:
+                        self.image = custom_img(self.image, 600, 16/9)
+                        
+                    if prev_instance.image and self.image != prev_instance.image:
+                        path = os.path.join(MEDIA_ROOT, prev_instance.image)
+                        if os.path.exists(path):
+                            os.remove(path)
+
+
+                if self.logo:
+                    if prev_instance.logo and self.logo != prev_instance.logo:
+                        path = os.path.join(MEDIA_ROOT, prev_instance.logo)
+                        if os.path.exists(path):
+                            os.remove(path)
+            except: pass
+        else:
+            if self.image:
+                self.image = custom_img(self.image, 600, (16/9))
+
+        super().save(*args, **kwargs)
+        return self
+
+
+    def delete(self, *args, **kwargs):
+        project_dir = os.path.join(MEDIA_ROOT, f'projects/{self.name.replace(" ", "-")}')
+        if os.path.exists(project_dir):
+            shutil.rmtree(project_dir)
+        return super().delete(*args, **kwargs)
     
     class Meta:
         ordering = ['order', 'name']
@@ -79,13 +118,42 @@ class ProjectImage(models.Model):
     def __str__(self):
         return self.project.name + ' - ' + str(self.image)
     
+    def save(self, *args, **kwargs):
+
+        if self.pk:
+            prev_instance = ProjectImage.objects.filter(pk=self.pk)
+            if prev_instance.exists():
+                prev_instance = prev_instance[0]
+                if self.image != prev_instance.image:
+                    self.image = custom_img(self.image)
+
+                if prev_instance.image and self.image != prev_instance.image:
+                    if (self.image and self.image != prev_instance.image) or self.type != 'image':
+                        path = os.path.join(MEDIA_ROOT, prev_instance.image)
+                        if os.path.exists(path):
+                            os.remove(path)
+        else:
+            if self.image:
+                self.image = custom_img(self.image)
+
+        super().save(*args, **kwargs)
+        return self
+    
+    def delete(self, *args, **kwargs):
+        if self.image:
+            path = os.path.join(MEDIA_ROOT, self.image)
+            if os.path.exists(path):
+                os.remove(path)
+
+        return super().delete(*args, **kwargs)
+    
     class Meta:
         ordering = ['project', 'section']
 
 
 class ProjectVideo(models.Model):
     def get_video_path(instance, filename):
-        return f'projects/{instance.project.name}/videos/{filename}'
+        return f'projects/{instance.project.name.replace(" ", "-")}/videos/{filename}'
 
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=1000, null=True, blank=True)
@@ -95,6 +163,29 @@ class ProjectVideo(models.Model):
 
     def __str__(self):
         return self.project.name + '//' + self.title
+    
+    def save(self, *args, **kwargs):
+    
+        if self.pk:
+            prev_instance = ProjectVideo.objects.filter(pk=self.pk)
+            if prev_instance.exists():
+                prev_instance = prev_instance[0]
+
+                if prev_instance.video and self.video != prev_instance.video:
+                    path = os.path.join(MEDIA_ROOT, prev_instance.video)
+                    if os.path.exists(path):
+                        os.remove(path)
+
+        super().save(*args, **kwargs)
+        return self
+    
+    def delete(self, *args, **kwargs):
+        if self.video:
+            path = os.path.join(MEDIA_ROOT, self.video)
+            if os.path.exists(path):
+                os.remove(path)
+
+        return super().delete(*args, **kwargs)
     
     class Meta:
         ordering = ['project', 'section', 'title']
@@ -130,5 +221,3 @@ class Snippet(models.Model):
         formatter = HtmlFormatter(style=self.style, full=False, noclasses=True)
         self.highlighted = highlight(self.code, lexer, formatter)   
         super().save(*args, **kwargs)
-
-
