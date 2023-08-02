1be9f02f9f70b19ed0c303b78f2c8dce2b17f2ff
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-08-01 22:03:37 -0600

    Add models to admin

--- blog/admin.py
+++ blog/admin.py
@@ -3,4 +3,8 @@ from . import models
 
 admin.site.register(models.Category)
 admin.site.register(models.Post)
-admin.site.register(models.Content)
+admin.site.register(models.Title)
+admin.site.register(models.Paragraph)
+admin.site.register(models.Snippet)
+admin.site.register(models.Image)
+admin.site.register(models.Video)
