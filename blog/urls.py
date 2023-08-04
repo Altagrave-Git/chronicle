59dbca85e53288ac328f7010db046fc9ea2607ba
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-08-04 08:23:34 -0600

    Mod url route structure

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.categories_view, name='blog_categories'),
-    path('<str:category>/', views.posts_view, name='blog_posts'),
-    path('<str:category>/<slug:slug>/', views.content_view, name='blog_contents'),
+    path('<str:category>/', views.category_view, name='blog_category'),
+    path('<str:category>/posts/', views.posts_view, name='blog_posts'),
+    path('<str:category>/posts/<slug:slug>/', views.post_view, name='blog_post'),
+    path('<str:category>/posts/<slug:slug>/<str:type>/', views.contents_view, name='blog_contents'),
+    path('<str:category>/posts/<slug:slug>/<str:type>/<int:id>/', views.content_view, name='blog_content')
 ]
