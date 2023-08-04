d7c74c6ce9cd9ec6fdd616e7f90b2d2638e1ba4b
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-08-04 08:24:23 -0600

    Seperate into list and detail views

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from rest_framework.response import Response
 from .serializers import CategorySerializer, PostSerializer, ContentSerializer, TitleSerializer, ParagraphSerializer, SnippetSerializer, ImageSerializer, VideoSerializer
-from .models import Category, Post, Title, Paragraph, Snippet, Image, Video
+from .models import Category, Post, Title, Paragraph, Snippet, Image, Video, STYLE_CHOICES, LANGUAGE_CHOICES
 from rest_framework.decorators import api_view, permission_classes, authentication_classes, parser_classes, renderer_classes
 from rest_framework import permissions
 from rest_framework import status
@@ -10,7 +10,7 @@ from rest_framework import renderers
 
 
 
-@api_view(['GET', 'POST', 'DELETE'])
+@api_view(['GET', 'POST'])
 @permission_classes([permissions.IsAuthenticatedOrReadOnly])
 @renderer_classes([renderers.JSONRenderer, renderers.BrowsableAPIRenderer])
 @parser_classes([parsers.JSONParser, parsers.FormParser, parsers.MultiPartParser])
@@ -31,17 +31,46 @@ def categories_view(request):
                 return Response(serializer.errors)
         else:
             return Response(status=status.HTTP_401_UNAUTHORIZED)
+    else:
+        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+    
+
+@api_view(['GET', 'PUT', 'DELETE'])
+@permission_classes([permissions.IsAuthenticatedOrReadOnly])
+@renderer_classes([renderers.JSONRenderer, renderers.BrowsableAPIRenderer])
+@parser_classes([parsers.JSONParser, parsers.FormParser, parsers.MultiPartParser])
+def category_view(request, category):
+    try:
+        instance = Category.objects.get(name=category)
+    except:
+        return Response(status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = CategorySerializer(instance)
+        data = serializer.data
+        data['endpoints'] = ['/posts/']
+        return Response(data)
+
+    elif request.method == 'PUT':
+        if request.user.is_superuser:
+            serializer = CategorySerializer(instance, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+            else:
+                return Response(serializer.errors)
+        else:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
     
     elif request.method == 'DELETE':
-        if request.users.is_superuser:
-            name = request.data.get('name')
-            Category.objects.filter(name=name).delete()
-            return Response(status=status.HTTP_200_OK)
+        if request.user.is_superuser:
+            instance.delete()
+            return Response(status.HTTP_200_OK)
         else:
             return Response(status.HTTP_401_UNAUTHORIZED)
         
     else:
-        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        return Response(status.HTTP_405_METHOD_NOT_ALLOWED)
     
 
 @api_view(['GET', 'POST', 'DELETE'])
@@ -88,11 +117,20 @@ def posts_view(request, category):
         return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
 
+TYPE_METHODS = {
+    'title': [Title, TitleSerializer],
+    'paragraph': [Paragraph, ParagraphSerializer],
+    'snippet': [Snippet, SnippetSerializer],
+    'image': [Image, ImageSerializer],
+    'video': [Video, VideoSerializer]
+}
+
+
 @api_view(['GET', 'POST', 'DELETE'])
 @permission_classes([permissions.IsAuthenticatedOrReadOnly])
 @renderer_classes([renderers.JSONRenderer, renderers.BrowsableAPIRenderer])
 @parser_classes([parsers.JSONParser, parsers.FormParser, parsers.MultiPartParser])
-def content_view(request, category, slug):
+def post_view(request, category, slug):
     try:
         post = Post.objects.get(slug=slug)
     except:
@@ -100,8 +138,9 @@ def content_view(request, category, slug):
     
     if request.method == 'GET':
         serializer = ContentSerializer(post)
-        
-        return Response(serializer.data)
+        data = serializer.data
+        data['endpoints'] = [f'/{key}/' for key in TYPE_METHODS]
+        return Response(data)
             
     elif request.method == 'POST':
         if request.user.is_superuser:
@@ -132,5 +171,71 @@ def content_view(request, category, slug):
     elif request.method == 'DELETE':
         if request.user.is_superuser:
             return Response(status=status.HTTP_200_OK)
+    else:
+        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+
+@api_view(['GET', 'POST'])
+@permission_classes([permissions.IsAuthenticatedOrReadOnly])
+@renderer_classes([renderers.JSONRenderer, renderers.BrowsableAPIRenderer])
+@parser_classes([parsers.JSONParser, parsers.FormParser, parsers.MultiPartParser])
+def contents_view(request, category, slug, type):
+
+    [model, serializer] = TYPE_METHODS.get(type)
+
+    if request.method == 'GET':
+        post = Post.objects.get(slug=slug)
+        content = model.objects.filter(post=post.id)
+        serializer = serializer(content, many=True)
+        return Response(serializer.data)
+    
+    elif request.method == 'POST':
+        if request.user.is_superuser:
+            serializer = serializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+            else:
+                return Response(serializer.errors)
+        else:
+            return Response(status.HTTP_401_UNAUTHORIZED)
+    else:
+        return Response(status.HTTP_405_METHOD_NOT_ALLOWED)
+    
+
+@api_view(['GET', 'PUT', 'DELETE'])
+@permission_classes([permissions.IsAuthenticatedOrReadOnly])
+@renderer_classes([renderers.JSONRenderer, renderers.BrowsableAPIRenderer])
+@parser_classes([parsers.JSONParser, parsers.FormParser, parsers.MultiPartParser])
+def content_view(request, category, slug, type, id):
+
+    [model, serializer] = TYPE_METHODS.get(type)
+
+    try:
+        content = model.objects.get(id=id)
+        if content.post.slug != slug:
+            return Response({'err_msg':'content, post mismatch'}, status=status.HTTP_400_BAD_REQUEST)
+    except:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    
+    if request.method == 'GET':
+        return Response(serializer(content).data)
+    
+    elif request.method == 'PUT':
+        if request.user.is_superuser:
+            serializer = serializer(content, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+            else:
+                return Response(serializer.errors)
+    
+    elif request.method == 'DELETE':
+        if request.user.is_superuser:
+            content.delete()
+            return Response(status.HTTP_200_OK)
+        else:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
     else:
         return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
