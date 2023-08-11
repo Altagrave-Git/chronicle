9580dce6afdef78bc2b7431abc67c9e3e0432e8d
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-08-10 23:17:34 -0600

    Add related posts url, content detail url

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('<str:category>/', views.category_view, name='blog_category'),
     path('<str:category>/posts/', views.posts_view, name='blog_posts'),
     path('<str:category>/posts/<slug:slug>/', views.post_view, name='blog_post'),
+    path('<str:category>/posts/<slug:slug>/related/', views.related_view, name='blog_related'),
     path('<str:category>/posts/<slug:slug>/<str:type>/', views.contents_view, name='blog_contents'),
-    path('<str:category>/posts/<slug:slug>/<str:type>/<int:id>/', views.content_view, name='blog_content')
+    path('<str:category>/posts/<slug:slug>/<str:type>/<int:id>/', views.content_view, name='blog_content'),
 ]
