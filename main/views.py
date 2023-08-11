e84c6da1132f67cd44911af43be835a5a26a75c0
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-08-10 23:16:48 -0600

    Add new url route hints to data

--- main/views.py
+++ main/views.py
@@ -1,23 +1,35 @@
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
-from django.contrib.sessions.models import Session
+from rest_framework.decorators import api_view, renderer_classes, permission_classes
+from rest_framework import renderers, parsers, permissions
 
 
 @api_view(['GET'])
+@renderer_classes([renderers.BrowsableAPIRenderer, renderers.JSONRenderer])
+@permission_classes([permissions.IsAuthenticatedOrReadOnly])
 def index(request):
     data = [
         'Hello, welcome to Chronicle API',
         {
-        'endpoints': {
-            'projects': '/projects/',
-            'project': '/projects/:id/',
-            'project_sections': '/projects/:id/sections/',
-            'project_section': '/projects/:id/sections/:id/',
-            'project_images': '/projects/:id/images/',
-            'project_image': '/projects/:id/images/:id/',
-            'snippets': '/projects/:id/code/',
-            'snippet': '/projects/:id/code/:id/',
-        }}
+            'project endpoints': {
+                'projects': '/projects/',
+                'project': '/projects/:id/',
+                'sections': '/projects/:id/sections/',
+                'section': '/projects/:id/sections/:id/',
+                'images': '/projects/:id/images/',
+                'image': '/projects/:id/images/:id/',
+                'snippets': '/projects/:id/code/',
+                'snippet': '/projects/:id/code/:id/',
+            }},
+        {
+            'blog endpoints': {
+                'index': '/blog/',
+                'categories': '/blog/:category/',
+                'posts': '/blog/:category/posts/',
+                'post': '/blog/:category/posts/:slug/',
+                'contents': '/blog/:category/posts/:slug/:type/',
+                'content': '/blog/:category/posts/:slug/:type/:id/'
+            }
+        }
     ]
 
     return Response(data)
