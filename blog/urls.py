227144695e28beada408520b8a9857a7cb8d1eb1
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-08-12 02:49:23 -0600

    Add changerelatedview url

--- blog/urls.py
+++ blog/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('<str:category>/posts/', views.posts_view, name='blog_posts'),
     path('<str:category>/posts/<slug:slug>/', views.post_view, name='blog_post'),
     path('<str:category>/posts/<slug:slug>/related/', views.related_view, name='blog_related'),
+    path('<str:category>/posts/<slug:slug>/related/<int:id>/', views.change_related_view, name='blog_change_related'),
     path('<str:category>/posts/<slug:slug>/<str:type>/', views.contents_view, name='blog_contents'),
     path('<str:category>/posts/<slug:slug>/<str:type>/<int:id>/', views.content_view, name='blog_content'),
 ]
