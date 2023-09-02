5abec5419d4281a7454016c2665bdc4d0c115900
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-09-02 06:19:14 -0600

    Fix url heirarchy

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.categories_view, name='blog_categories'),
+    path('recent/', views.recent_posts, name="recent_posts"),
     path('<str:category>/', views.category_view, name='blog_category'),
     path('<str:category>/posts/', views.posts_view, name='blog_posts'),
     path('<str:category>/posts/<slug:slug>/', views.post_view, name='blog_post'),
@@ -10,5 +11,4 @@ urlpatterns = [
     path('<str:category>/posts/<slug:slug>/related/<int:id>/', views.change_related_view, name='blog_change_related'),
     path('<str:category>/posts/<slug:slug>/<str:type>/', views.contents_view, name='blog_contents'),
     path('<str:category>/posts/<slug:slug>/<str:type>/<int:id>/', views.content_view, name='blog_content'),
-    path('recent/', views.recent_posts, name="recent_posts")
 ]
