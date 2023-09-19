8bd7e4f7c1f93fe92d5ac8c3c29a6d18c4c1e6a9
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-09-18 22:27:29 -0600

    Add template directory

--- main/settings.py
+++ main/settings.py
@@ -74,7 +74,7 @@ SESSION_COOKIE_SAMESITE = None
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
